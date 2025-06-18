4a0ea2c736cbb18cff05678a45db6d2cbce050a3
JureTic <tic.jur@gmail.com>
2025-06-18 11:12:10 +0200

    Bug fix

--- gls-woo.php
+++ gls-woo.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: MyGLS WooCommerce Integration
  * Description: Integrates MyGLS API with WooCommerce (Paketomat support).
- * Version: 1.0.10
+ * Version: 1.0.11
  * Author: Tauria
  */
 
